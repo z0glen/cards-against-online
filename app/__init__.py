31ae32d721a43d6b5a4b0c3d9e5b068ca1606f4c
z0glen <zgbauer@gmail.com>
2020-05-06 19:41:56 -0400

    feat - login screen

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+import os
 
 app = Flask(__name__)
+
+app.secret_key = os.environ.get('SECRET_KEY', 'dev')
+app.password = os.environ.get('CARDS_PASSWORD', 'dev')
+
+bootstrap = Bootstrap(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 
 from app import app
+from app.forms import LoginForm
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return "Hello, World!"
+    form = LoginForm()
+    if form.validate_on_submit() and form.password.data == app.password:
+        flash('Logged in')
+        return redirect(url_for('game'))
+    flash('test')
+    return render_template("home.html", form=form)
+
+@app.route("/game")
+def game():
+    return render_template("game.html")
