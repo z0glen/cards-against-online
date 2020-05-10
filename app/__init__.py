77081b97ca1123cc85b549238ddf46921367ec51
z0glen <zgbauer@gmail.com>
2020-05-10 04:23:16 -0400

    vue tutorial

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
@@ -8,5 +9,6 @@ app.secret_key = os.environ.get('SECRET_KEY', 'dev')
 app.password = os.environ.get('CARDS_PASSWORD', 'dev')
 
 bootstrap = Bootstrap(app)
+cors = CORS(app, resources={r'/*': {'origins': '*'}})
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,29 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, jsonify, request
+import uuid
 
 from app import app
 from app.forms import LoginForm
+
+BOOKS = [
+    {
+        'id': uuid.uuid4().hex,
+        'title': 'On the road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'id': uuid.uuid4().hex,
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J.K. Rowling',
+        'read': False
+    },
+    {
+        'id': uuid.uuid4().hex,
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
@@ -15,3 +37,48 @@ def home():
 @app.route("/game")
 def game():
     return render_template("game.html")
+
+@app.route("/ping")
+def ping():
+    return jsonify('pong!')
+
+@app.route('/books', methods=['GET', 'POST'])
+def all_books():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book added!'
+    else:
+        response_object['books'] = BOOKS
+    return jsonify(response_object)
+
+@app.route('/books/<book_id>', methods=['PUT', 'DELETE'])
+def single_book(book_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_book(book_id)
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book updated!'
+    if request.method == 'DELETE':
+        remove_book(book_id)
+        response_object['message'] = 'Book removed!'
+    return jsonify(response_object)
+
+def remove_book(book_id):
+    for book in BOOKS:
+        if book['id'] == book_id:
+            BOOKS.remove(book)
+            return True
+    return False
