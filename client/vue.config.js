9be13001c838a154a8229880f4e742a4e26645a8
z0glen <zgbauer@gmail.com>
2020-06-18 17:23:12 -0400

    wip: heroku ugh

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-    proxy: {
-        '/socket.io': {
-            target: `http://${process.env.VUE_APP_FLASK_HOST}:5000`,
-            ws: true,
-            changeOrigin: true
+    devServer: {
+        proxy: {
+            '/socket.io': {
+                target: `http://${process.env.VUE_APP_FLASK_HOST}:5000`,
+                ws: true,
+                changeOrigin: true
+            }
         }
     }
 };
