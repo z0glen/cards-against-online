cdddab00db89100864b05de0dcbf5a1d4dd2bb23
z0glen <zgbauer@gmail.com>
2020-07-02 16:50:45 -0400

    fix: travis deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-docker tag z0glen/cards-against-online registry.heroku.com/still-fortress-86423/web
 docker push registry.heroku.com/still-fortress-86423/web
 heroku container:release --app still-fortress-86423 web
