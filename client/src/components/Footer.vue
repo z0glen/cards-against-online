9dfc5ff5a57fe27cdf48f1ebbde166f211d57288
z0glen <zgbauer@gmail.com>
2020-07-05 16:46:06 -0400

    oops forgot to move version

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -4,6 +4,9 @@
             Source code and issue/feature tracking on
             <a href="https://github.com/z0glen/cards-against-online" target="_blank" rel="noopener noreferrer">GitHub</a>
         </b-nav-text>
+        <b-navbar-nav class="ml-auto">
+            <b-nav-text right>Version 0.1.5</b-nav-text>
+        </b-navbar-nav>
     </b-navbar>
 </template>
 
--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,12 +1,6 @@
 <template>
     <b-navbar toggleable="lg" type="dark" variant="info">
         <b-navbar-brand to="/">Cards Against Online</b-navbar-brand>
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-        <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav class="ml-auto">
-                <b-nav-text>Version 0.1.4</b-nav-text>
-            </b-navbar-nav>
-        </b-collapse>
     </b-navbar>
 </template>
 
