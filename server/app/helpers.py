5a8c5c7e72c49e9466ce0cce05fbd66eefc77dd5
z0glen <zgbauer@gmail.com>
2020-08-17 13:26:26 -0400

    feat: add helper for printing calls text

--- server/app/helpers.py
+++ server/app/helpers.py
@@ -11,4 +11,17 @@ def get_json(url):
 def read_file(path):
     f = open(path, "r")
     contents = f.read()
+    f.close()
     return json.loads(contents)
+
+
+def join_cards():
+    calls = read_file('calls.json')
+    f = open('joined_cards.txt', "w")
+    for c in calls:
+        f.write("%s\n" % '_____'.join(c['text']))
+    f.write("\n\n")
+    responses = read_file('responses.json')
+    for r in responses:
+        f.write("%s\n" % '_____'.join(r['text']))
+    f.close()
