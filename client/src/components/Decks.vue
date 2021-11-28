b7101e64b1b53bf3bd9bdf33fbd6cf3424f4407d
z0glen <zgbauer@gmail.com>
2021-11-28 18:06:20 -0500

    clean: move form to new component

--- client/src/components/Decks.vue
+++ client/src/components/Decks.vue
@@ -23,83 +23,12 @@
         </template>
         <div v-else>
             <h3>Deck: {{ this.selectedDeckId }}</h3>
-            <b-button v-b-modal.create-card variant="primary" class="button">Create Card</b-button>
-            <b-modal
+            <b-button v-b-modal.create-card variant="primary" class="button" @click="isCreateCardModalOpen = true">Create Card</b-button>
+            <CreateCardModal
                 id="create-card"
-                title="Create Card"
-                okTitle="Create"
-                @ok="handleCreateCard"
-            >
-                <b-form>
-                    <b-form-group
-                        label="Card Type"
-                        description="Black cards contain blanks, white cards fill in those blanks"
-                    >
-                        <b-form-radio-group
-                            v-model="selectedCardType"
-                            :options="cardTypeOptions"
-                            button-variant="outline-dark"
-                            buttons
-                        />
-                    </b-form-group>
-                    <b-form-group
-                        label="Card Content"
-                        :description="contentDescription"
-                    >
-                        <template v-if="selectedCardType">
-                            <template v-if="selectedCardType === 'black'">
-                                <b-button
-                                    v-if="selectedCardText[0] === '_____'"
-                                    size="sm"
-                                    @click="selectedCardText.unshift('')"
-                                >
-                                    Add Text
-                                </b-button>
-                                <b-button
-                                    v-else
-                                    size="sm"
-                                    @click="selectedCardText.unshift('_____')"
-                                >
-                                    Add Blank
-                                </b-button>
-                            </template>
-                            <div
-                                v-for="(el, ind) in selectedCardText"
-                                :key="`cardElement${ind}`"
-                                style="margin: 10px 0px;"
-                            >
-                                <span
-                                    v-if="el === '_____'"
-                                >
-                                    __(space for white card)__
-                                </span>
-                                <b-form-input
-                                    v-else
-                                    v-model="selectedCardText[ind]"
-                                    lazy-formatter
-                                    :formatter="formatter"
-                                />
-                            </div>
-                            <template v-if="selectedCardType === 'black'">
-                                <b-button
-                                    v-if="selectedCardText[selectedCardText.length - 1] === '_____'"
-                                    size="sm"
-                                    @click="selectedCardText.push('')"
-                                >
-                                    Add Text
-                                </b-button>
-                                <b-button
-                                    v-else
-                                    size="sm"
-                                    @click="selectedCardText.push('_____')"
-                                >
-                                    Add Blank
-                                </b-button>
-                            </template>
-                        </template>
-                    </b-form-group>
-                </b-form>
-            </b-modal>
+                @refresh="handleRefresh"
+                @error="handleError"
+            />
             <b-card-group
                 deck
                 style="margin: 25px 0px;">
@@ -125,16 +54,14 @@
 
 <script>
     import Card from "./Card.vue";
+    import CreateCardModal from "./forms/CreateCardModal.vue";
 
     export default {
         components: {
-            Card
+            Card,
+            CreateCardModal
         },
         data() {
-            const CARD_TYPE_OPTIONS = [
-                { text: "Black", value: "black" },
-                { text: "White", value: "white" },
-            ]
             return {
                 decks: [],
                 selectedDeck: {},
@@ -142,23 +69,10 @@
                 error: "",
                 alertCountdown: 0,
                 dismissSecs: 10,
-                cardTypeOptions: CARD_TYPE_OPTIONS,
-                selectedCardType: null,
-                selectedCardText: null,
             }
         },
         created() {
             this.fetchDecks()
-        },
-        computed: {
-            contentDescription () {
-                if (this.selectedCardType === "black") {
-                    return "Black cards contain multiple parts with at least one blank. Use the 'Add Text' and 'Add Blank' buttons to add additional parts"
-                } else if (this.selectedCardType === "white") {
-                    return "White cards are one piece of text with no blanks";
-                }
-                return "Select a card type before adding content";
-            }
         },
         methods: {
             fetchDecks() {
@@ -190,42 +104,17 @@
                 this.selectedDeckId = name;
                 this.selectedDeck = deck;
             },
-            handleCreateCard () {
-                console.log(this.selectedCardText);
-                return fetch('http://localhost:5000/card/create', {
-                    method: 'POST',
-                    headers: {
-                        'content-type': 'application/json'
-                    },
-                    body: JSON.stringify({cardType: this.selectedCardType, cardContent: this.selectedCardText})
-                }).then(res => {
-                    if (!res.ok) {
-                        console.error('error creating new card');
-                        throw new Error(res.statusText);
-                    }
-                    return res.json();
-                }).then(json => {
-                    console.log(json);
-                    this.decks = json;
-                }).catch(err => {
-                    this.error = err;
-                });
+            handleRefresh(json) {
+                this.decks = json;
             },
-            formatter (value) {
-                return value + " ";
+            handleError(error) {
+                this.error = error;
             }
         },
         watch: {
             error(newError) {
                 if (newError) {
                     this.alertCountdown = this.dismissSecs;
-                }
-            },
-            selectedCardType (newType) {
-                if (newType === 'black') {
-                    this.selectedCardText = ["_____"];
-                } else {
-                    this.selectedCardText = [""];
                 }
             }
         }
